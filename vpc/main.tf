c640c7675c24520562bebb054ab23d096de24aa2
IremKarakoyun <125840202+IremKarakoyun@users.noreply.github.com>
2024-10-27 12:57:01 -0500

    modified main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -14,18 +14,3 @@ module "vpc" {
   }
 }
 
-output private_subnets {
-    value = module.vpc.private_subnets
-}
-
-output public_subnets {
-    value = module.vpc.public_subnets
-}
-
-output vpc_id {
-    value = module.vpc.vpc_id
-}
-
-output launch_template_arn {
-    value = module.vpc.launch_template_arn
-}
